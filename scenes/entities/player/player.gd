77359272d9de15399b2a969f7922af2a9b7d8abf
Michael Janeway <michael.janeway03@gmail.com>
2025-05-24 15:54:55 -0400

    Switched to Rockin Plants

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -6,16 +6,19 @@ class_name Player
 
 func _physics_process(delta):
 	var direction = Vector2(Input.get_axis('ui_left', 'ui_right'), Input.get_axis('ui_up', 'ui_down')).normalized()
-	direction *= delta * 200
+	direction *= delta * 150
+	
+	char_sprite.flip_h = false
 	
 	# Don't move if no inputs
 	if direction.length() > 0:
 		position += direction
 		if abs(direction.x) > abs(direction.y):
 			if direction.x > 0:
-				animation_player.play("Right")
+				animation_player.play("Side")
 			else:
-				animation_player.play("Left")
+				char_sprite.flip_h = true
+				animation_player.play("Side")
 		else:
 			if direction.y > 0:
 				animation_player.play("Down")
